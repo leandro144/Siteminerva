9335d22c652266a8219b5ae3f66f86220bae3965
LeandroCastro <leandro_castro980@hotmail.com>
2024-05-15 17:28:54 -0300

    fim

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,8 +3,8 @@
 const Footer = () => {
   return (
     <>
-        <footer id="contato">
-            <p>Contato: contatominervaconstrucoes@gmail.com</p>
+        <footer>
+            <p id="contato">Contato: contatominervaconstrucoes@gmail.com</p>
             <h4>Criado e Desenvolvido por LeandroCastro - 2024</h4>
         </footer>
     </>
